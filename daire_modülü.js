fdc8185fb817b2945feb6a11fa2760c55d87acb1
Nisa Yılankırkan <90152455+nisa6677@users.noreply.github.com>
2021-10-24 15:04:08 +0300

    Add files via upload

---
+++ daire_modülü.js
@@ -0,0 +1,12 @@
+const circleArea = (radius) => {
+    let alan = Math.PI(radius **2)
+    console.log(alan);
+}
+const circleCircumFerence = (radius) => {
+    let circleCircumFerence = Math.round(radius.Math.PI*2)
+    console.log(circleCircumFerence);
+}
+module.exports = {
+    circleArea,
+    circleCircumFerence
+}
